f1ae8a54d30fe5e233a77ec89df003d664bc1d5d
TanyaIgnatenko <alisainy@gmail.com>
2019-04-21 22:24:21 +0700

    Update moment ru locale

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import moment from 'moment';
 
 import { App } from './containers/app';
 import { Calendar } from './components/common/Calendar';
 
 import './assets/scss/main.scss';
+
+moment.locale('ru');
+moment.updateLocale('ru', {
+  months: [
+    'Январь',
+    'Февраль',
+    'Март',
+    'Апрель',
+    'Май',
+    'Июнь',
+    'Июль',
+    'Август',
+    'Сентябрь',
+    'Октябрь',
+    'Ноябрь',
+    'Декабрь',
+  ],
+});
 
 const history = createBrowserHistory();
 
