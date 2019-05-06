316cda812740e9a367549c78397926bd5684a31a
TanyaIgnatenko <alisainy@gmail.com>
2019-05-06 19:39:20 +0700

    Add styles for spinner

--- src/components/app-components/common/Spinner/Spinner.js
+++ src/components/app-components/common/Spinner/Spinner.js
@@ -5,7 +5,8 @@ import './Spinner.scss';
 function Spinner() {
   return (
     <div className='info-box'>
-      <div className='info loading'>Загружаются...</div>
+      <div className='spinner' />
+      <p className='loading-msg'>Идет загрузка</p>
     </div>
   );
 }
